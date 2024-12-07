4840ef2bce17adcd2b34ee41b493ecbd9cf58a54
priyavenkat23 <shanmugapriya.gunaseelan@gmail.com>
2024-12-06 20:36:09 -0600

    Added Hero section and about me

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import About from './components/About';
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Hero/>
+     <About/>
     </div>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import HeroImg from '../assets/priya.png';
+import { AiOutlineTwitter, AiOutlineFacebook, AiOutlineLinkedin } from "react-icons/ai";
+
+export default function Hero() {
+    const config  = {
+        subtitle: 'Full-stack developer',
+        social: {
+            twitter: '',
+            facebook: '',
+            linkedin: 'https://www.linkedin.com/in/shanmugapriya-gunaseelan-989378115/'
+        }
+    }
+
+
+    return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
+       <div className='md:w-1/2 flex flex-col'>
+            <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> I'm Shanmugapriya Gunaseelan
+                <p className='text-2xl'>{config.subtitle}</p>
+            </h1>
+            <div className='flex py-10'>
+                <a href={config.social.twitter} className='pr-5 hover:text-white'><AiOutlineTwitter size={40} /></a>
+                <a href={config.social.facebook} className='pr-5 hover:text-white'><AiOutlineFacebook size={40}/></a>
+                <a href={config.social.linkedin} className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
+            </div>
+       </div>
+       <img className='md:w-1/4' src={HeroImg} />
+    </section>
+}
