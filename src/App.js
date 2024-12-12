dcf97c00a12a168a575afbb39ac50aa8bea9c084
priyavenkat23 <shanmugapriya.gunaseelan@gmail.com>
2024-12-11 20:31:09 -0600

    added resume content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import About from './components/About';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
      <Header/>
      <Hero/>
      <About/>
+     <Projects/>
+     <Resume/>
     </div>
   );
 }
